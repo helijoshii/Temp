ef78a85d31240ec0aa11bf274e36678a3db00a6a
MannThakar14 <76089480+MannThakar@users.noreply.github.com>
2024-08-27 02:52:51 +0530

    Without navbar

--- js/general.js
+++ js/general.js
@@ -1,6 +1,6 @@
 let currentSlide = 0;
-let slides = document.querySelectorAll('.carousel-item');
-let indicators = document.querySelectorAll('.indicator');
+let slides = document.querySelectorAll(".carousel-item");
+let indicators = document.querySelectorAll(".indicator");
 
 function nextSlide() {
   currentSlide = (currentSlide + 1) % slides.length;
@@ -18,11 +18,11 @@ function goToSlide(index) {
 }
 
 function updateCarousel() {
-slides.forEach((slide, index) => {
-   slide.classList.toggle('active', index === currentSlide);
+  slides.forEach((slide, index) => {
+    slide.classList.toggle("active", index === currentSlide);
   });
-indicators.forEach((indicator, index) => {
-   indicator.classList.toggle('active', index === currentSlide);
+  indicators.forEach((indicator, index) => {
+    indicator.classList.toggle("active", index === currentSlide);
   });
 }
 
@@ -31,42 +31,50 @@ setInterval(nextSlide, 3000);
 updateCarousel();
 
 var $j = jQuery.noConflict();
-$j('.hero-slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true
-  });
+$j(".hero-slider").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+});
 
-$j('.company-slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 900,
-    arrows: false,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
+$j(".company-slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 900,
+  arrows: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
   autoplaySpeed: 2000,
-  });
+});
 
-  $j('.company-slider-reverse').slick({
-    dots: false,
-    infinite: true,
-    speed: 900,
-    arrows: false,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
+$j(".company-slider-reverse").slick({
+  dots: false,
+  infinite: true,
+  speed: 900,
+  arrows: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
   autoplaySpeed: 2000,
-  rtl: true
-  });
+  rtl: true,
+});
 
-  $j('.testimonial-slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    arrows: true,
-    autoplay: true,
+$j(".testimonial-slider").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: true,
+  autoplay: true,
   autoplaySpeed: 2000,
-  });
+});
+function toggleDrawer() {
+  const drawer = document.getElementById("drawer");
+  if (drawer.style.left === "0px") {
+    drawer.style.left = "-250px";
+  } else {
+    drawer.style.left = "0px";
+  }
+}
